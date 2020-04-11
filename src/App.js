c4654f27852f96a558ded3b4d41a2db3aab85023
Alex Taylor (alta) <alta@fb.com>
2020-04-11 16:19:00 -0700

    playing los master plus

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyClient from './spotify.js';
 
 function App() {
   const clientID = 'd73f9dfa97c44b57ac7cefcc031c4df9';
@@ -8,14 +9,33 @@ function App() {
   const redirectURI = encodeURIComponent('http://localhost:3000/');
   const authorizeURI = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientID}&scope=${scopes}&redirect_uri=${redirectURI}`;
 
+  const [accessToken, setAccessToken] = React.useState(null);
+
   if (window.location.hash) {
     const params = {};
     window.location.hash.slice(1).split('&').map(param => param.split('=')).forEach(tup => { params[tup[0]] = tup[1] });
     const accessToken = params.access_token;
-    console.log(accessToken);
-    // Save this somewhere
+    setAccessToken(accessToken);
+    console.log('setaccesstoken');
     window.location.hash = '';
   }
+
+  React.useMemo(() => {
+    console.log('inhere');
+    console.log(accessToken);
+    if (accessToken) {
+      console.log('initing');
+      const client = new SpotifyClient(accessToken);
+      client
+        .prepareSpotifyClient()
+        .then(() => client.fetchState())
+        .then((data) => {
+          console.log(data);
+          client.play('spotify:track:727LbE4pV6RtLK5FnH1WIe');
+        });
+    }
+  }, [accessToken]); // Only re-run the effect if count changes
+
 
   return (
     <div className="App">
