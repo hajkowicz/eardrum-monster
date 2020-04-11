32e87dac0229e95781fdb765824a41cde5aa2ff4
Alex Taylor (alta) <alta@fb.com>
2020-04-11 15:47:11 -0700

    Access token logged

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const clientID = 'd73f9dfa97c44b57ac7cefcc031c4df9';
+  const scopes = 'streaming+user-read-email+user-read-private+user-read-playback-state+user-modify-playback-state+user-read-currently-playing';
+  const redirectURI = encodeURIComponent('http://localhost:3000/');
+  const authorizeURI = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientID}&scope=${scopes}&redirect_uri=${redirectURI}`;
+
+  if (window.location.hash) {
+    const params = {};
+    window.location.hash.slice(1).split('&').map(param => param.split('=')).forEach(tup => { params[tup[0]] = tup[1] });
+    const accessToken = params.access_token;
+    console.log(accessToken);
+    // Save this somewhere
+    window.location.hash = '';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-title">EARDRUM MONSTER</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href={authorizeURI}>Login with Spotify</a>
       </header>
+      <div>
+        <h2>now playing</h2>
+      </div>
     </div>
   );
 }
