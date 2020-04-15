b54e05185ce4c41380e36b88eef1efe5d209d7b3
Alex Taylor (alta) <alta@fb.com>
2020-04-15 15:43:36 -0700

    styling

--- src/Home.js
+++ src/Home.js
@@ -21,29 +21,61 @@ const UserList = ({ users }) => (
 function Home() {
   return (
     <div className="Home">
-      <Connect
-        query={graphqlOperation(queries.listUsers, {
-          limit: 50,
-        })}
-        subscription={graphqlOperation(subscriptions.onUpdateUser)}
-        onSubscriptionMsg={(prev, { onUserUpdate }) => {
-          // TODO update user list here
-          return prev;
-        }}
-      >
-        {({ data, loading, error }) => {
-          if (error) return <h3>Error</h3>;
-          if (loading || !data) return <h3>Loading...</h3>;
-          const users = (data.listUsers && data.listUsers.items) ?? [];
-          return (
-            <>
-              {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
-              <h2 className="Home-title">😈 MONSTER LIST 😈</h2>
-              <UserList users={users} />
-            </>
-          );
-        }}
-      </Connect>
+      <div className="Home-content">
+        <div>
+          <em>
+            eardrum.monster allows you to sync your spotify client with friends
+          </em>
+        </div>
+        <h1>How do I use it?</h1>
+        <div>1. Click "Broadcast" at the top ☝️</div>
+        <div>2. Share the URL with your friends️</div>
+        <h1>Its great for:</h1>
+        <div>
+          ✅ Streamers who want to let their viewers sync up their music with
+          high quality spotify audio
+        </div>
+        <div>✅ Online gamers who want listen together while gaming</div>
+        <div>
+          ✅ People who are quarantined and want to have virtual power hours
+          together
+        </div>
+
+        <h1>Did you say power hour?</h1>
+        <div>
+          ✅ Yes! eardrum.monster now features <em>Power hour mode™️</em> that
+          will automatically change the song every 60s. Everybody must drink
+          when the song changes.
+        </div>
+
+        <h1>Can I play century club?</h1>
+        <div>
+          ✅ We've got you covered! <em>Power hour mode™️</em> will keep em'
+          coming well beyond the traditional 60 minutes so you can attempt to
+          join the century club as many times as you want!
+        </div>
+      </div>
+      <div className="Home-list">
+        {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
+        <h2 className="Home-title">😈 MONSTER LIST 😈</h2>
+        <Connect
+          query={graphqlOperation(queries.listUsers, {
+            limit: 50,
+          })}
+          subscription={graphqlOperation(subscriptions.onUpdateUser)}
+          onSubscriptionMsg={(prev, { onUserUpdate }) => {
+            // TODO update user list here
+            return prev;
+          }}
+        >
+          {({ data, loading, error }) => {
+            if (error) return <h3>Error</h3>;
+            if (loading || !data) return <h3>Loading...</h3>;
+            const users = (data.listUsers && data.listUsers.items) ?? [];
+            return <UserList users={users} />;
+          }}
+        </Connect>
+      </div>
     </div>
   );
 }
